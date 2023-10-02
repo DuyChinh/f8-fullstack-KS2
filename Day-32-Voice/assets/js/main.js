fa82a92cad688b71264a6c56c10ddf51232b2366
DuyChinh <doanduychinh2102@gmail.com>
2023-10-02 12:28:14 +0700

    add map and yt

--- Day-32-Voice/assets/js/main.js
+++ Day-32-Voice/assets/js/main.js
@@ -9,8 +9,63 @@ recognition.lang = 'vi-VI';
 //return immediately data
 recognition.continuous = false;
 
+const standardString= (content, mark) => {
+    let res = "";
+    const arr = content.split(" ");
+    if(arr.length < 2) {
+        return content;
+    }
+    arr.forEach(value => {
+        res+=value+ mark;
+    });
+    return res.slice(0, res.length-mark.length);
+}
+
 const handleVoice = (text) => {
-    console.log(text);
+    switch (text) {
+        case 'youtube': 
+            window.open("https://www.youtube.com/");
+            break;
+    }
+
+    if(text.includes("video")) {
+        const content = text.slice(text.indexOf("video")+5).trim();
+        if(content === "") {
+            window.open("https://www.youtube.com/");
+        } else {
+            const res = standardString(content, '+');
+            window.open(`https://www.youtube.com/results?search_query=${res}`);
+        }
+    }
+    else if(text.includes("tới") || text.includes("đường")) {
+        if(text.indexOf("tới") >= 0) {
+            const content = text.slice(text.indexOf("tới")+3).trim();
+            const res = standardString(content, "%20");
+            window.open(`https://www.google.com/maps/search/${res}`);
+        } else if(text.indexOf("đường") >= 0) {
+            const content = text.slice(text.indexOf("tới")+3).trim();
+            const res = standardString(content, "%20");
+            window.open(`https://www.google.com/maps/search/${res}`);
+        }
+    }
+
+    // if(text === 'google') {
+    //     window.open("https://google.com.vn");
+    // } else if(text === 'facebook') {
+    //     window.open("https://facebook.com");
+
+    // } else if(text === 'google drive') {
+    //     window.open("https://drive.google.com/drive/my-drive");
+        
+    // } else if(text === 'google maps') {
+    //     window.open("https://www.google.com/maps");
+        
+
+    // } else if(text === 'youtube') {
+    //     window.open("https://www.youtube.com/");
+    // }
+    statusAction.textContent = `Đã thực hiện xong`;
+
 }
 
 mic.addEventListener("click", (e) => {
@@ -39,7 +94,9 @@ recognition.addEventListener("result", (e) => {
     // console.log(text);
     statusAction.textContent = `Đang thực hiện: ${text}`;
     statusAction.style.display = "block";
-    handleVoice(text);
+    setTimeout(()=> {
+        handleVoice(text);
+    }, 1000)
 });
 
 
