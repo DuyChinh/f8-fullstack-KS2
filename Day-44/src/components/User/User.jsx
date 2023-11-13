88a5b413cd701a21df6816defb94028f0e52ec33
Duy Chinh <35019449+doanlinh9524@users.noreply.github.com>
2023-11-13 12:43:34 +0700

    Day-44

--- Day-44/src/components/User/User.jsx
+++ Day-44/src/components/User/User.jsx
@@ -1,17 +1,81 @@
 import { useAuth0 } from "@auth0/auth0-react"
-import { useState } from "react";
-import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+// import { useState } from "react";
+import Loading from "../Loading/Loading";
+import SignOut from "../SignOut/SignOut"
+import emailjs from "@emailjs/browser";
+import { toast } from "react-toastify";
 
+const serviceId = import.meta.env.VITE_ID_SERVICE_EMAILJS;
+const temId = import.meta.env.VITE_ID_TEMPLATE_EMAILJS;
+const apiKeys = import.meta.env.VITE_KEY_EMAILJS;
 
 const User = ()=> {
-    // const {isLoading, isAuthenticated, user} = useAuth0();
+    const {isLoading, user, isAuthenticated} = useAuth0();
     // const {loading, setLoading} = useState(false);
-    
-    // console.log(user.name);
+    const postEmail = (e) => {
+      e.preventDefault();
+      // setLoading(true);
+
+      emailjs.sendForm(
+        serviceId,
+        temId,
+        e.target,
+        apiKeys
+      )
+      .then(
+        (response) => {
+          console.log(response);
+          toast.success(`Send success`)
+        },
+        (error) => {
+          console.log(error);
+          toast.error(`Send fail!`);
+        }
+      )
+      
+    }
     return (
-      <div>
-        
-      </div>
+      <>
+        {isAuthenticated && (
+          <div className="container">
+            <div className="user-info">
+              <img src={user.picture} alt={user.name} className="avatar" />
+              <h2>Hello: {user.name}</h2>
+              {user.email && (
+                <p>
+                  Email: <a href={`mailto:${user.email}`}>{user.email}</a>
+                </p>
+              )}
+            </div>
+
+            <form action="" className="form-response" onSubmit={postEmail}>
+              <label htmlFor="email">Email của bạn* </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                required
+                placeholder="Nhập email của bạn"
+                defaultValue={user.email}
+              />
+              <label htmlFor="message">Tin nhắn </label>
+              <textarea
+                type="text"
+                name="message"
+                id="message"
+                required
+                placeholder="Nhập tin nhắn của bạn"
+                defaultValue="Tôi cần trợ giúp bài tập về nhà!"
+                cols="40"
+                rows="10"
+              ></textarea>
+              <button type="submit" className="btn">Yêu cầu hỗ trợ</button>
+            </form>
+            <SignOut/>
+            {(isLoading) && <Loading/>}
+          </div>
+        )}
+      </>
     );
 }
 
